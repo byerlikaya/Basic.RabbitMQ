a82f0e12426bb084e0abca2e20353181d8e54c87
Baris Yerlikaya <b.yerlikaya@outlook.com>
2024-06-25 18:23:39 +0300

    Added ServiceLifetime parameter for MessageConsumer.

--- src/Basic.RabbitMQ/Extensions/ServiceCollectionExtensions.cs
+++ src/Basic.RabbitMQ/Extensions/ServiceCollectionExtensions.cs
@@ -1,22 +1,61 @@
 ﻿namespace Basic.RabbitMQ.Extensions;
 
+// ReSharper disable once UnusedType.Global
 public static class ServiceCollectionExtensions
 {
-    public static void AddRabbitMQClient(this IServiceCollection services, IConfiguration configuration)
-    {
-        var brokerOptions = configuration.GetSection(nameof(MessageBrokerOptions)).Get<MessageBrokerOptions>();
+    // ReSharper disable once UnusedMember.Global
+    public static void AddRabbitMqClient(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        ServiceLifetime messageConsumerServiceLifetime = ServiceLifetime.Singleton) =>
+        CreateServices(services, configuration.GetSection(nameof(MessageBrokerOptions)).Get<MessageBrokerOptions>(), messageConsumerServiceLifetime);
 
+    // ReSharper disable once UnusedMember.Global
+    public static void AddRabbitMqClient(
+        this IServiceCollection services,
+        MessageBrokerOptions messageBrokerOptions,
+        ServiceLifetime messageConsumerServiceLifetime = ServiceLifetime.Singleton) =>
+        CreateServices(services, messageBrokerOptions, messageConsumerServiceLifetime);
+
+    private static void CreateServices(
+        IServiceCollection services,
+        MessageBrokerOptions messageBrokerOptions,
+        ServiceLifetime messageConsumerServiceLifetime)
+    {
         services.AddSingleton(_ => new ConnectionFactory
         {
-            HostName = brokerOptions.HostName,
-            UserName = brokerOptions.Username,
-            Password = brokerOptions.Password,
-            VirtualHost = brokerOptions.VirtualHost,
-            DispatchConsumersAsync = true
+            HostName = messageBrokerOptions.HostName,
+            UserName = messageBrokerOptions.Username,
+            Password = messageBrokerOptions.Password,
+            VirtualHost = messageBrokerOptions.VirtualHost,
+            DispatchConsumersAsync = true,
+            AutomaticRecoveryEnabled = true,
+            NetworkRecoveryInterval = TimeSpan.FromSeconds(30)
         });
 
-        services.AddSingleton<RabbitMQClientService>();
-        services.AddSingleton<IMessageProducer, MessageProducer>();
+        services.AddSingleton<RabbitMqClientService>();
         services.AddSingleton<IMessageConsumer, MessageConsumer>();
+        CreateConsumerService(services, messageConsumerServiceLifetime);
+    }
+
+    private static void CreateConsumerService(
+        IServiceCollection services,
+        ServiceLifetime messageConsumerServiceLifetime)
+    {
+        switch (messageConsumerServiceLifetime)
+        {
+            case ServiceLifetime.Singleton:
+                services.AddSingleton<IMessageProducer, MessageProducer>();
+                break;
+            case ServiceLifetime.Scoped:
+                services.AddScoped<IMessageProducer, MessageProducer>();
+                break;
+            case ServiceLifetime.Transient:
+                services.AddTransient<IMessageProducer, MessageProducer>();
+                break;
+            default:
+                services.AddSingleton<IMessageProducer, MessageProducer>();
+                break;
+        }
     }
 }

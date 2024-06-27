e836641a6d5182a1e7254a7aed18c8858255147f
Baris Yerlikaya <b.yerlikaya@outlook.com>
2024-06-27 16:05:19 +0300

    Removed connection creation.

--- src/Basic.RabbitMQ/Services/RabbitMQClientService.cs
+++ src/Basic.RabbitMQ/Services/RabbitMQClientService.cs
@@ -1,17 +1,8 @@
 ﻿namespace Basic.RabbitMQ.Services;
 
-public class RabbitMqClientService(IConfiguration configuration, ConnectionFactory connectionFactory)
+public class RabbitMqClientService(IConfiguration configuration)
 {
     public readonly MessageBrokerOptions BrokerOptions = configuration.GetSection(nameof(MessageBrokerOptions)).Get<MessageBrokerOptions>();
-
-    private IConnection _connection;
-
-    public IModel Connect(string queueName)
-    {
-        if (_connection is not { IsOpen: true })
-            _connection = connectionFactory.CreateConnection();
-        return CreateChannel(_connection, queueName);
-    }
 
     public IModel Connect(
         IConnection connection,
